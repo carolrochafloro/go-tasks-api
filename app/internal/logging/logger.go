55c48cb8e03d170371e11bcbb34f0d0dad5d1497
Carolina Floro <carolinarochaf@hotmail.com>
2024-07-23 13:10:17 -0300

    Fixed logger.go to create new logging file in the same directory

--- app/internal/logging/logger.go
+++ app/internal/logging/logger.go
@@ -4,13 +4,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var Logger *log.Logger
 
 func NewLogger() {
 
-	logFile, err := os.OpenFile(os.Getenv("LOG_FILE"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	logDir := filepath.Join("app", "internal", "logging")
+	logFilePath := filepath.Join(logDir, "app.log")
+
+	// Cria o diretório se não existir
+	if err := os.MkdirAll(logDir, os.ModePerm); err != nil {
+		log.Fatal("Failed to create log directory:", err)
+		panic("Failed to create log directory")
+	}
+
+	logFile, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
 	if err != nil {
 		log.Fatal("Failed to open log file:", err)
