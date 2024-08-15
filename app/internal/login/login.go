f91021b644e30b0a77b12c0b81bca8702ed9d655
Carolina Floro <carolinarochaf@hotmail.com>
2024-08-15 20:27:40 -0300

    Refactor login handler

--- app/internal/login/login.go
+++ app/internal/login/login.go
@@ -1,26 +1,14 @@
 package login
 
 import (
-	"context"
 	"encoding/json"
-	"go-tasks-api/app/internal/db"
-	"go-tasks-api/app/internal/user"
+	"go-tasks-api/app/internal/logging"
+	"go-tasks-api/app/internal/middleware"
+	"go-tasks-api/app/internal/utils"
 	"net/http"
-	"os"
-	"time"
-
-	"github.com/golang-jwt/jwt/v5"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
-
-	godotenv.Load()
-
-	client := db.Client
 	
 	if r.Header.Get("Content-Type") != "application/json" {
         http.Error(w, "Content type is not application/json", http.StatusUnsupportedMediaType)
@@ -43,50 +31,21 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return 
 	}
 
-	collection := client.Database("go-tasks").Collection("users")
-
-	email := login["email"]
-	password := login["password"]
-
-	filter := bson.D{{Key: "email", Value: email}}
-
-	var user user.UserT
-
-	err = collection.FindOne(context.TODO(), filter).Decode(&user)
+	jwt, err := middleware.Authenticate(login)
 
 	if err != nil {
-
-		if err == mongo.ErrNoDocuments {
-			http.Error(w, "Usuário não encontrado", http.StatusUnauthorized)
-            return
-		}
-
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusUnauthorized)
+		logging.Error("Unable to authenticate", err)
+		utils.RespondWithError(w, http.StatusBadRequest, "Unable to authenticate.")
 		return
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"id": user.Id,
-		"nbf": time.Now().Unix(),
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
-	})
-
-	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_KEY")))
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusUnauthorized)
-		return
-	}
-
-	w.Header().Set("Authorization", "Bearer "+tokenString)
+	w.Header().Set("content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Successful login"))
+
+	response := 
+	map[string]string{"message:":"Success",
+						"jwt:": jwt,}
+	jsonResponse, _ := json.Marshal(response)
+	w.Write(jsonResponse)
 
 }
